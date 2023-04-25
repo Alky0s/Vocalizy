7b0704f71a1eecbec857169557971363126cf863
Kevin Teillier <kt.teillier@gmail.com>
2023-04-25 02:22:53 +0200

    pictures modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ import Footer from '../components/Footer';
 import Singer from '../assets/chant1.jpg';
 import Singer2 from '../assets/chant2.jpg';
 import Singer3 from '../assets/chant3.jpg';
+import concert1 from '../assets/concert1.jpg';
+import concert2 from '../assets/concert2.jpg';
+import concert3 from '../assets/concert3.jpg';
 import { Link } from 'react-router-dom';
 
 
@@ -24,9 +27,9 @@ const Home = () => {
             <section className='singer'>
                 <h3>Un concert chaque année pour s'exprimer devant son public !</h3>
                 <div className='singer-pictures'>
-                    <img className="" src={Singer} alt='chanteur élève'></img>
-                    <img className="" src={Singer2} alt='chanteur élève'></img>
-                    <img className="" src={Singer3} alt='chanteur élève'></img>
+                    <img className="" src={concert1} alt='chanteur élève'></img>
+                    <img className="" src={concert2} alt='chanteur élève'></img>
+                    <img className="" src={concert3} alt='chanteur élève'></img>
                 </div>
             </section>
             <button className='singer-contact'><Link to={"/join"}>Trouvez votre "voix" !</Link></button>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import cours from '../assets/cours-chant.jpg';
+import sing from '../assets/sing.jpg';
+import duo from '../assets/duo.jpg';
 
 const Services = () => {
     return (
@@ -10,13 +11,13 @@ const Services = () => {
             <p className='services-p'>La voix est un instrument vivant, et demande un échauffement adapté. Éveil corporel, mise en voix, travail postural et de respiration, technique vocale... <br /> Cours adpatés à chaque élève de tous niveaux.</p>
             <div className='services'>
                 <article className="services_article">
-                    <img className="services_article-img" src={cours} alt="chanteuse" />
+                    <img className="services_article-img" src={sing} alt="chanteuse" />
                     <h3 className="services_article-title">Cours de chant individuel</h3>
                     <h4 className="services_article-subtitle">30€ l'heure de cours.</h4>
                     <h4 className="services_article-subtitle">Plusieurs formules disponibles.</h4>
                 </article>
                 <article className="services_article">
-                    <img className="services_article-img" src={cours} alt="chanteuse" />
+                    <img className="services_article-duo" src={duo} alt="duo" />
                     <h3 className="services_article-title">Cours de chant DUO</h3>
                     <h4 className="services_article-subtitle">45€ l'heure de cours ou 60€ pour 1h30.</h4>
                 </article>
