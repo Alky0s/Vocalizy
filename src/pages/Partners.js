6f607ee23935d9ca80dab0d88912dce9743711b6
Kevin Teillier <kt.teillier@gmail.com>
2023-04-26 01:04:15 +0200

    responsive desktop done

--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Partners = () => {
     return (
         <div>
-            Partenaires
+            <Header />
+            <Footer />
         </div>
     );
 };
